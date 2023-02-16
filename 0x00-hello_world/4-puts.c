238fab494ac880dcca53e4a0fe16dea928670351
muftisanni <muftisanni@gmail.com>
2023-02-16 13:15:18 -0800

    Low level programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Strating point
+ *
+ * puts - main method
+ *
+ * Return: exit point
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * printf - main function
+ *
+ * Return: Exit point
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
