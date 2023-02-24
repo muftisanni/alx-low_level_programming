fcbdfc27d429b29eaadfc701b078e7991e8cc732
muftisanni <muftisanni@gmail.com>
2023-02-23 22:32:51 -0800

    0-isupper.c is committed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,4 @@
 #include "main.h"
-
-/**
- * isupper detects upper case charatcers
- *@c: This is the character required to be checked
- *Return: if c is upper case, 0 otherwise
- */
 int _isupper (int c)
 
 {
