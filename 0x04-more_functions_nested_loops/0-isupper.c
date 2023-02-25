4275abe31391ae347c1e2a69451bb494093c0c9c
muftisanni <muftisanni@gmail.com>
2023-02-24 19:05:02 -0800

    0-isupper file committed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,10 @@
 #include "main.h"
 int _isupper (int c)
-
+/**
+ * _isupper tells if there is an Uppercase character
+ *@c:The character you are to check.
+ * Return:1 if c is uppercase, 0 otherwise.
+ */
 {
 	return (c >= 'A' && c <= 'Z';
 }	
