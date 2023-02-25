c19b5a0783f126ec441d1e077fc823687c833e8a
muftisanni <muftisanni@gmail.com>
2023-02-24 19:33:45 -0800

    1-isdigit.c file committed

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit checks for digits 0 through 9
+ * @c: is the digit to be checked.
+ * Return: 1 if c is digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+
+{	return (c >= '0' && c <= '9');
+
+}	
